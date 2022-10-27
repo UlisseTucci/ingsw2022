72dcfa08af1f7cfd197b7eee7fd064bed89c57fd
Ulisse Tucci <ulissetucci@gmail.com>
2022-10-27 16:55:55 +0200

    Initial commit

---
+++ JUnitTest2021/src/test/java/it/unical/ingsw2021/MyMathTest.java
@@ -0,0 +1,105 @@
+package it.unical.ingsw2021;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.Rule;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import it.unical.ingsw2021.ConnectionManager;
+import it.unical.ingsw2021.MyMath;
+
+import static org.mockito.Mockito.*;
+
+public class MyMathTest {
+
+	private static MyMath myMath;
+	
+	@Mock
+	private ConnectionManager connectionManagerMock;
+	@Rule
+	public MockitoRule mockitoRule = MockitoJUnit.rule();
+	
+	@BeforeClass
+	public static void prepareAll() {
+		System.out.println("before class");
+		//myMath = new MyMath();
+	}
+
+	@AfterClass
+	public static void afterClass() {
+		System.out.println("after class");
+	}
+
+	@Before
+	public void prepareTest() {
+		System.out.println("before");
+		myMath = new MyMath(connectionManagerMock);
+	}
+
+	@After
+	public void cleanTest() {
+		System.out.println("after");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void fibonacciThrowsException() {
+		System.out.println("fibonacciThrowsException");
+		myMath.fibonacci(-1);
+	}
+
+	@Test
+	public void fibonacciWorks() {
+		System.out.println("testing that fibonacci works");
+		assertEquals(0, myMath.fibonacci(0));
+		assertEquals(1, myMath.fibonacci(1));
+		assertEquals(5, myMath.fibonacci(5));
+		assertEquals(8, myMath.fibonacci(6));
+		assertEquals(102334155, myMath.fibonacci(40));
+	}
+
+	@Test
+	public void factorialWorks() {
+		System.out.println("testing that factorial works");
+		assertEquals(1, myMath.factorial(0));
+		assertEquals(120, myMath.factorial(5));
+	}
+
+	@Test(timeout = 5000)
+	public void fibonacciIsFastEnough() {
+		System.out.println("fibonacciIsFastEnough");
+		myMath.fibonacci(40);
+	}
+
+	@Test
+	public void testOnDoubles() {
+		System.out.println("testOnDoubles");
+		assertEquals(0.3, 0.1 + 0.1 + 0.1, 0.001);
+	}
+	
+	/**
+	 * Just to show that we can modify the Mocked instance behavior
+	 */
+	@Test
+	public void remoteFactorialWorks() {
+		System.out.println("testing that remoteFactorial Works");	
+		when(connectionManagerMock.isNetworkConnected()).thenReturn(true);
+		assertEquals(12345678, myMath.remoteFactorial(2));
+	}
+
+	/**
+	 * Just to show that we can modify the Mocked instance behavior
+	 */
+	@Test(expected = RuntimeException.class)
+	public void remoteFactorialThrowsException() {
+		System.out.println("testing that remoteFactorial throws");	
+		when(connectionManagerMock.isNetworkConnected()).thenReturn(false);
+		myMath.remoteFactorial(5);
+	}
+	
+}
---
+++ src/main/java/it/unical/ingsw2021/MyMath.java
@@ -0,0 +1,64 @@
+package it.unical.ingsw2021;
+
+public class MyMath {
+
+	private ConnectionManager cm;
+
+	/**
+	 * Creating dependency within the class makes testing difficult, 
+	 * it might be better to receive the dependency from outside
+	 */
+	public MyMath() {}
+	
+	/**
+	 * This constructor is used to receive the ConnectionManager dependency
+	 * (Set methods could also be used)
+	 */
+	public MyMath(ConnectionManager cm) {		
+		this.cm = cm;
+	}
+
+	/**
+	 * Calculates the fibonacci series until it finds the nth number in the series
+	 * @param n 	the nth position
+	 * @return the nth number in the fibonacci series 
+	 */
+	public int fibonacci(int n) {		
+		if (n < 0) {
+			throw new IllegalArgumentException("Illegal value " + n);
+		}
+		if (n == 0) {
+			return 0;
+		}
+		if (n == 1) {
+			return 1;
+		}
+		return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+
+	/**
+	 * Calculates the factorial of a given number
+	 * @param n 	the number to calculate the factorial
+	 * @return the factorial for a given integer
+	 */
+	public int factorial(int n) {
+
+		if (n == 0) {
+			return 1;
+		}
+		return n * factorial(n - 1);
+	}
+
+	/**
+	 * This method is to simulate a network connection manager
+	 * @param n		the number to calculate the factorial
+	 * @return the factorial calculated remotely 
+	 */
+	public int remoteFactorial(int n) {
+		if(!cm.isNetworkConnected()) { // Se non siamo connessi ad internet...
+			throw new RuntimeException("It's not posible to reach the server");
+		}
+		return 12345678;
+	}
+
+}
